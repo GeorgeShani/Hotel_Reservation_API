cdf2f240b1a21f8a0c6429990782c5e4bbde8123
GeorgeShani <shanidze.geo2004@gmail.com>
2025-04-22 18:54:34 +0400

    Add Room, Reservation and Payment controllers and initialize environment variables via .env

--- src/test/java/com/example/hotel_reservation_api/HotelReservationApiApplicationTests.java
+++ src/test/java/com/example/hotel_reservation_api/HotelReservationApiApplicationTests.java
@@ -5,9 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HotelReservationApiApplicationTests {
-
     @Test
-    void contextLoads() {
-    }
-
+    void contextLoads() {}
 }
