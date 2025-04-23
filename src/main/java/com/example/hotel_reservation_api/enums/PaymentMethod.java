22ab1843d7060353b56753ba11dc429a394f292e
GeorgeShani <shanidze.geo2004@gmail.com>
2025-04-23 19:38:41 +0400

    Update available values for PaymentMethod enum

--- src/main/java/com/example/hotel_reservation_api/enums/PaymentMethod.java
+++ src/main/java/com/example/hotel_reservation_api/enums/PaymentMethod.java
@@ -2,5 +2,13 @@ package com.example.hotel_reservation_api.enums;
 
 public enum PaymentMethod {
     CARD,
-    CASH
+    CASH,
+    BANK_TRANSFER,
+    APPLE_PAY,
+    GOOGLE_PAY,
+    SAMSUNG_PAY,
+    PAYPAL,
+    CRYPTOCURRENCY,
+    VOUCHER,
+    INVOICE
 }
