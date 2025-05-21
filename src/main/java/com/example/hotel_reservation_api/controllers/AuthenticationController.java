d56c2b8f1c78a7d720fbbec5eaa13ca280ad4bc9
GeorgeShani <shanidze.geo2004@gmail.com>
2025-05-21 17:42:14 +0400

    Resolve unicode recognition problems

--- src/main/java/com/example/hotel_reservation_api/controllers/AuthenticationController.java
+++ src/main/java/com/example/hotel_reservation_api/controllers/AuthenticationController.java
@@ -3,6 +3,7 @@ package com.example.hotel_reservation_api.controllers;
 import com.example.hotel_reservation_api.requests.post.LoginRequest;
 import com.example.hotel_reservation_api.requests.post.RegisterRequest;
 import com.example.hotel_reservation_api.services.AuthenticationService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,7 +35,9 @@ public class AuthenticationController {
     @PostMapping("/login")
     public ResponseEntity<?> logIn(@RequestBody @Valid LoginRequest request) {
         try {
-            return ResponseEntity.ok(authenticationService.logIn(request));
+            return ResponseEntity.ok()
+                    .contentType(MediaType.valueOf("application/json;charset=UTF-8"))
+                    .body(authenticationService.logIn(request));
         } catch (RuntimeException ex) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("message", ex.getMessage()));
         }
