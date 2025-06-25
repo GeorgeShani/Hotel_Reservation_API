83eaffb4fe57476aea1f70a8a322adbaad163384
GeorgeShani <shanidze.geo2004@gmail.com>
2025-06-25 18:43:45 +0400

    Remove Redundant packages from ActuatorConfig

--- src/main/java/com/example/hotel_reservation_api/configs/ActuatorConfiguration.java
+++ src/main/java/com/example/hotel_reservation_api/configs/ActuatorConfiguration.java
@@ -1,10 +1,8 @@
 package com.example.hotel_reservation_api.configs;
 
-import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.actuate.info.InfoContributor;
-import org.springframework.boot.actuate.info.Info;
 
 @Configuration
 public class ActuatorConfiguration {
